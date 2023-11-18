a3974dd7a06254ab3fb7df340ace14f112c78c7e
root <+>
2023-11-18 22:58:14 +0300

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+        <li>
+          <Link to="/directors">Directors</Link>
+        </li>
+        <li>
+          <Link to="/actors">Actors</Link>
+        </li>
+      </ul>
+      <h1>Home Page</h1>
+    </div>
+  );
 }
 
 export default Home;
